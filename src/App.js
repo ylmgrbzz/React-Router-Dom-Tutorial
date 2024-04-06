6edc24fde39efe4d49c54f6904a137aba4523923
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-06 13:58:34 +0300

    Add Users and UserDetails components to App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import NoMatch from "./components/NoMatch";
 import Product from "./components/Product";
 import Featured from "./components/Featured";
 import NewProduct from "./components/NewProduct";
+import Users from "./components/Users";
+import UserDetails from "./components/UserDetails";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
           <Route path="featured" element={<Featured />} />
           <Route path="new" element={<NewProduct />} />
         </Route>
+        <Route path="users" element={<Users />} />
+        <Route path="users/:userId" element={<UserDetails />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
---
+++ src/components/Users.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Users = () => {
+  return (
+    <div>
+      Users
+      <ul>
+        <li>user1</li>
+        <li>user2</li>
+        <li>user3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
