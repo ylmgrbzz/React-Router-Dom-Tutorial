e894e4239f26c60b2c6cce7d64f6b8f9cbbcf892
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-06 13:19:09 +0300

    Update Navbar styling and import in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import Navbar from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
-const Navbar = () => {
+export const Navbar = () => {
+  const navStyles = ({ isActive }) => {
+    return {
+      color: isActive ? "red" : "blue",
+      fontWeight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "underline" : "none",
+    };
+  };
+
   return (
     <nav>
       {/* <Link to="/">Home</Link>
       <Link to="/about">About</Link>    */}
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+      <NavLink style={navStyles} to="/">
+        Home
+      </NavLink>
+      <NavLink style={navStyles} to="/about">
+        About
+      </NavLink>
     </nav>
   );
 };
-
-export default Navbar;
