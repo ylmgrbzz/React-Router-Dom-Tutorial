3f071488c6296fca26e4c5f47055f6e1c09235a5
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-06 13:35:55 +0300

    Add NoMatch component to handle 404 routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import { Navbar } from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
   );
