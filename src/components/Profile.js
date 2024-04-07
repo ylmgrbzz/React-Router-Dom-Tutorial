49638c89d915078accdda453ba1c541d61578621
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-07 16:43:19 +0300

    Add user profile functionality

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useAuth } from "./Auth";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const navigate = useNavigate();
+  const { user, logout } = useAuth();
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p> welcome {user}</p>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 };
 
 export default Profile;
